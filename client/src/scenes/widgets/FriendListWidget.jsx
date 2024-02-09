71e38d083301e79fc0cde66310ad69244c2822e8
rohithreddy <yrohithreddy12@gmail.com>
2024-02-10 04:18:38 +0530

    added backend urls

--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -13,7 +13,7 @@ const FriendListWidget = ({ userId }) => {
 
   const getFriends = async () => {
     const response = await fetch(
-      `http://localhost:3001/users/${userId}/friends`,
+      `https://mernsocialmediaapp-production.up.railway.app/${userId}/friends`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
