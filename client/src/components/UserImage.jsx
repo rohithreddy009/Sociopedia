a309e960964e6d404f638399cd92e8a030b8c966
rohithreddy <yrohithreddy12@gmail.com>
2024-02-10 15:19:38 +0530

    Revert "added backend urls"
    This reverts commit 71e38d083301e79fc0cde66310ad69244c2822e8.
    
    unnecessary commit, broke my code

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -8,7 +8,7 @@ const UserImage = ({ image, size = "60px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={`https://mernsocialmediaapp-production.up.railway.app/${image}`}
+        src={`http://localhost:3001/assets/${image}`}
       />
     </Box>
   );
--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -13,7 +13,7 @@ const FriendListWidget = ({ userId }) => {
 
   const getFriends = async () => {
     const response = await fetch(
-      `https://mernsocialmediaapp-production.up.railway.app/${userId}/friends`,
+      `http://localhost:3001/users/${userId}/friends`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
