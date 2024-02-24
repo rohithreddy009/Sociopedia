5e09fe0c3c61a491318af06f2b3f173c0a5af930
rohithreddy <yrohithreddy12@gmail.com>
2024-02-24 16:09:48 +0530

    vercel backend deployment

--- server/index.js
+++ server/index.js
@@ -36,12 +36,6 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors());
 
-
-// app.use(cors())
-
-
-
-
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
